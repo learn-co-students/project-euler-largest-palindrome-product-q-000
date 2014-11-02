09999d4853da836fa1d92016080648b32295451c
irmiller22 <irmiller22@gmail.com>
2014-11-02 18:13:47 -0500

    updated environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ require 'pry'
 
 Bundler.require(:default)
 
-require './lib/even_fibonacci'
-require './lib/oo_even_fibonacci'
+require './lib/largest_palindrome_product'
+require './lib/oo_largest_palindrome_product'
