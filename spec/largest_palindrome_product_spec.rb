e361023f0ba0ad13f9ca86685580087a4154ded3
irmiller22 <irmiller22@gmail.com>
2014-11-02 18:13:58 -0500

    updated specs

---
+++ spec/largest_palindrome_product_spec.rb
@@ -0,0 +1,5 @@
+describe "#largest_palindrome_product" do
+  it 'returns the largest palindrome product of 2 3 digit numbers' do
+    expect(largest_palindrome_product).to eq(906609)
+  end
+end
---
+++ spec/oo_largest_palindrome_product_spec.rb
@@ -0,0 +1,5 @@
+describe "LargestPalindromeProduct" do
+  it 'returns the largest palindrome product of 2 3 digit numbers' do
+    expect(LargestPalindromeProduct.new.answer).to eq(906609)
+  end
+end
