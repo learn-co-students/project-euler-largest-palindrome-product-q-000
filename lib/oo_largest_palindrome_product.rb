41171911ed058d982d0a065aaa5a83ad3c2a441e
azheng249 <andy.zheng249@gmail.com>
2015-12-18 16:48:56 +0000

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,12 @@
 # Implement your object-oriented solution here!
+
+require_relative 'largest_palindrome_product'
+
+class LargestPalindromeProduct
+  attr_accessor :answer
+  
+  def initialize
+    @answer = largest_palindrome_product
+  end
+  
+end
