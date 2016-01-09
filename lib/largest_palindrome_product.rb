36836036113ffb292000d36e54e8b84f20dc6376
alexbarron <alexbarron@gmail.com>
2016-01-09 12:17:41 -0800

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+
+def largest_palindrome_product
+  x = 999
+  set = (100..999).to_a
+  largest_palindrome_product = 0
+
+  while x >= 100
+    set.each do |i|
+      product = x * i
+      product_array = product.to_s.chars.map(&:to_i)
+      if product_array == product_array.reverse && x * i > largest_palindrome_product
+        largest_palindrome_product = product
+      end
+    end
+    x -= 1
+  end
+  return largest_palindrome_product
+end
+
