f5ce40d3e530d9d275836917f373d726b3035dc6
Clay McCullough <clay44@kc.rr.com>
2016-02-21 23:51:45 +0000

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def answer
+    max = 0
+    999.downto(100) do |i|
+      i.downto(100) do |j|
+        result = i * j
+        max = result if result > max && result.to_s == result.to_s.reverse
+        break if result <= max
+      end
+    end
+    max
+  end
+end
