9c5a97b7df536970dc4910bcd9b0df70604f8cde
bmolina-nyc <bruce.d.molina@gmail.com>
2016-02-28 09:59:33 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,24 @@
 # Implement your procedural solution here!
+
+
+def is_palindrome?(number)
+  i = 0 
+  number = number.to_s.split("")
+  while i < number.length
+    return false if number[i] != number[(-1)-i]
+    i += 1 
+  end
+  true 
+end
+
+
+def largest_palindrome_product
+arr = [] 
+  999.downto(100).each do |num|
+      999.downto(100).each do |num2|
+        test_num = num * num2 
+        arr << test_num if is_palindrome?(test_num)
+      end
+    end
+    arr.sort.last
+end
