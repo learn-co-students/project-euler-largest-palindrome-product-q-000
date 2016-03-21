0a5ce2ef2b063e717acab4f8730fd07902d5aa46
einarstensson <einar.stensson@gmail.com>
2016-03-21 17:05:54 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,28 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  first_operand = 999
+  second_operand = 999
+  largest_palindrome = 0
+
+  until first_operand == 100 && second_operand < 100
+    product = first_operand * second_operand
+
+    largest_palindrome = product if palindrome?(product) && 
+                                    product > largest_palindrome
+
+    if second_operand > 99
+      second_operand -= 1
+    else
+      second_operand = 999
+      first_operand -= 1
+    end
+  end
+
+  largest_palindrome
+end
+
+def palindrome?(product)
+  string_product = product.to_s
+
+  return string_product if string_product == string_product.reverse
+  false
+end
