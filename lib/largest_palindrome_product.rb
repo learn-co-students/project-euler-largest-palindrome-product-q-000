f43fe74ce3089db2e8b6be38d0079d1816f38d65
anorico <allan.norico@gmail.com>
2016-03-28 12:24:47 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+
+  palindrome = []
+
+  array = 111.upto(999)
+  counter = 1
+
+  array.each do |x|
+    array.each do |y|
+      n = x * y
+      counter += 1
+      if n == n.to_s.reverse.to_i
+        palindrome << n
+        #p "match! test #{counter} | #{n} == #{n.to_s.reverse.to_i} "
+      end
+    end
+  end
+  p palindrome.max
+  palindrome.max
+end
