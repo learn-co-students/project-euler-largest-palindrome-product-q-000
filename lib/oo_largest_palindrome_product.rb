2ae96a453a6e6c76167e0bf285a6833706f16ffe
Yohanes Wibowo <john.paulo@ymail.com>
2016-05-16 21:14:53 +0700

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,29 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def initialize
+    @answer = palindrome_product
+    @palindrome1 = 0
+    @palindrome2 = 0
+  end
+
+  def is_palindrome?(bil)
+    bil.to_s == bil.to_s.reverse
+  end
+
+  def palindrome_product
+    maks = 0
+    (100..999).each do |a1|
+      (100..999).each do |a2|
+        kali = a1 * a2
+        if is_palindrome?(kali) && kali > maks
+          maks = kali
+        end
+      end
+    end
+    maks
+  end
+
+  def answer
+    @answer
+  end
+end
