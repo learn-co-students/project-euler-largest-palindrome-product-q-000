0c2034a8faa12e4a439cc89af050c24d04e290a0
Pamela Alvarez <p.stacey.a@gmail.com>
2016-07-12 01:57:59 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,39 @@
 # Implement your procedural solution here!
+
+
+=begin
+
+# just my trail & fail error of figuring it out.
+
+def largest_palindrome_product
+  max = 0
+
+     999.downto(100) do |a|
+       a.downto(100) do |b|
+         answer = a * b
+
+     if answer.to_s == answer.to_s.reverse && answer < max then max = answer end
+
+      puts answer
+end
+end
+end
+
+array = []
+
+def palindrome?(number)
+  number.to_s == number.to_s.reverse
+end
+
+=end
+
+def largest_palindrome_product(min = 100, max = 999 )
+ palindrome = []
+  (min...max).each do |a|
+    (min...max).each do |b|
+      product = a * b
+   palindrome << product if product.to_s == product.to_s.reverse
+ end
+     end
+   palindrome.max
+   end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+  def palindrome?(number)
+    number.to_s == number.to_s.reverse
+  end
+
+  def answer(min = 100, max = 999)
+    palindrome = []
+     (min...max).each do |a|
+       (min...max).each do |b|
+         product = a * b
+      palindrome << product if palindrome?(product)
+    end
+        end
+      palindrome.max
+      end 
+  end
