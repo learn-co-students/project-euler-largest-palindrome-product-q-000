3e137887d585de6e492ed585de44d944752fd9fd
Olia Tkachenko <liaolya@ukr.net>
2017-03-09 17:45:54 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def reverse_int( n )
+    n.to_s.reverse.to_i
+end
+
+def largest_palindrome_product()
+a = Array.new
+for i in 900..999
+for k in 900..999
+p=i*k
+if p == reverse_int(p)
+  puts p
+  a.push(p)
+end
+end
+end
+return a.max
+end
