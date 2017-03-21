99e783cf5752876ca74330647d8a3fedcc8f7276
SiamKing <thesiameseelectric@gmail.com>
2017-03-21 14:32:29 -0700

    your mom is a palindrome

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  x = y = 999
+
+  while x > 900 do
+    while y > x - 100
+      a = (x * y).to_s
+      break if a == a.reverse
+      y -= 1
+    end
+    break if a == a.reverse
+    y += 99
+    x -= 1
+  end
+  x * y
+end
