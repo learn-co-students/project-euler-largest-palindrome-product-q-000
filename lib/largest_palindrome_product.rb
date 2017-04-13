a4353cf811b66606f1c9d8ce5e36570091251fb7
sami4thewin <samiomarmohammed@gmail.com>
2017-04-12 22:43:43 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+
+def largest_palindrome_product
+  y = 0
+  (100..999).to_a.each do |num|
+    (100..999).to_a.each do |n|
+
+      ans = num * n
+      if ans.to_s == ans.to_s.reverse && ans > y
+        y = ans
+      end
+    end
+  end
+  # binding.pry
+  #   n = 999 * num
+  #   if n.to_s == n.to_s.reverse
+  #     y = num
+  #   end
+  # end
+  y
+end
