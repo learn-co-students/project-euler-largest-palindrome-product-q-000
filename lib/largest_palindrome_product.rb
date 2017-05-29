9ef30297da191f0d7adc4b8178d951b635ddf538
mirandalily <mirandaraymond@me.com>
2017-05-29 12:29:37 -0700

    problem setup

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,3 @@
 # Implement your procedural solution here!
+def is_palindrome?(input)
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,9 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def initialize
+  end
+
+  def is_palindrome?
+  end
+end
