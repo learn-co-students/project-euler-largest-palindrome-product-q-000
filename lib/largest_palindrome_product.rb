3599ff18333db78c6b6d66506fc00ad08636129e
mirandalily <mirandaraymond@me.com>
2017-06-05 13:37:40 -0700

    1 test passing of 2

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,3 +1,16 @@
 # Implement your procedural solution here!
 def palindrome?(input)
+  input.to_s == input.to_s.reverse
+end
+
+def largest_palindrome_product
+  palindromes = []
+
+  (100..999).each do |x|
+    (100..999).each do |y|
+      product = x * y
+      (palindromes << product) if palindrome?(product)
+    end
+  end
+  palindromes.max
 end
