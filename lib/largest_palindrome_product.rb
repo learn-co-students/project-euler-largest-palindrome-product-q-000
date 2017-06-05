c7c666cc400acabea0f38f7afa217d55cdc19288
mirandalily <mirandaraymond@me.com>
2017-06-05 13:40:48 -0700

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,5 +1,5 @@
 # Implement your procedural solution here!
-def palindrome?(input)
+def is_palindrome?(input)
   input.to_s == input.to_s.reverse
 end
 
@@ -9,7 +9,7 @@ def largest_palindrome_product
   (100..999).each do |x|
     (100..999).each do |y|
       product = x * y
-      (palindromes << product) if palindrome?(product)
+      (palindromes << product) if is_palindrome?(product)
     end
   end
   palindromes.max
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -5,12 +5,12 @@ class LargestPalindromeProduct
     input.to_s == input.to_s.reverse
   end
 
-  def result
+  def answer
     palindromes = []
     (100..999).each do |x|
       (100..999).each do |y|
         product = x * y
-        (palindromes << product) if self.palindrome?(product)
+        (palindromes << product) if self.is_palindrome?(product)
       end
     end
     palindromes.max
