c29ac8f7cc96cc2b6d717c6cbaf5f3f8bb508bb4
mirandalily <mirandaraymond@me.com>
2017-06-05 13:34:54 -0700

    finished product file

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,10 +1,19 @@
 # Implement your object-oriented solution here!
 class LargestPalindromeProduct
 
-  def is_palindrome?
+  def is_palindrome?(number)
+    number.to_s == number.to_s.reverse
   end
 
   def result
+    palindromes = []
+    (100..999).each do |x|
+      (100..999).each do |y|
+        product = x * y
+        (palindromes << product) if self.palindrome?(product)
+      end
+    end
+    palindromes.max
   end
 
 end
