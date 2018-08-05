0e795a41d8b9c2e98be790343eacd80a8fceff8b
Bob Habig <rfhab2@gmail.com>
2018-08-04 23:10:53 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+require 'pry'
+def largest_palindrome_product
+   pal_max = 0
+   
+   (100..999).to_a.reverse.each do |n|
+      (100..999).to_a.reverse.each do |nTwo|
+         sixDigit = (n * nTwo).to_s
+         if sixDigit == sixDigit.reverse && sixDigit.to_i > pal_max
+            pal_max = sixDigit.to_i
+         end
+      end
+   end
+  
+   pal_max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,16 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+   def answer
+      pal_max = 0
+   
+      (100..999).to_a.reverse.each do |n|
+         (100..999).to_a.reverse.each do |nTwo|
+            sixDigit = (n * nTwo).to_s
+            if sixDigit == sixDigit.reverse && sixDigit.to_i > pal_max
+               pal_max = sixDigit.to_i
+            end
+         end
+      end
+   
+      pal_max
+   end
+end
