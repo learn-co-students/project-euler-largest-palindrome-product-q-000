a31bae687844dc278e04d3a074dfce7b9a0e8a2c
Sara Stanton <sara.mpa13@gmail.com>
2019-03-31 23:51:29 -0500

    euler problem 4

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+  first_set = Array(100..999)
+  second_set = Array(100..999)
+  first_set.each |x|
+    second_set.each |y|
+      product = (x * y).to_s
+      if product.reverse == product
+        return product
+      end
+    end
+  end
+end
+
+
+
+#
+# def return_largest_pal_product(num1, num2)
+#   find_pal_products(num1, num2).max
+# end
